a221b77d01b4ec88c575588537bc0d7e6bd84aa3
Lloyd Rhodes <lloydrhodes@hotmail.co.uk>
2020-05-09 08:36:13 +0100

    remove docker

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ io.on('connection', (socket) => {
   })
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 9090, () => {
+  console.log('app listening', process.env.PORT);
 });
